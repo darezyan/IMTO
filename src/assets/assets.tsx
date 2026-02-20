a23cf0855695f4ef38c9534aa6143da936c3e51a
Michael Omaruaye <michyb113@gmail.com>
2026-02-20 16:35:33 +0100

    ode

---
+++ src/assets/assets.tsx
@@ -0,0 +1,7 @@
+import blackBackground from "@/assets/images/blackbackground.png";
+import logo from "@/assets/images/PrimaryLogo.png";
+
+export const assets = {
+  PrimaryLogo: logo,
+  BlackBackground: blackBackground,
+};
